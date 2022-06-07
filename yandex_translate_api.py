75f490f4636ed921cf72d5bd3c648459eef00cc2
Sz3rs <96138257+Sz3rs@users.noreply.github.com>
2022-06-07 16:46:23 +0600

    Update yandex_translate_api.py
    fix and add async

--- yandex_translate_api.py
+++ yandex_translate_api.py
@@ -1,16 +1,45 @@
 import requests
+import aiohttp
+
+url = 'https://browser.translate.yandex.net/api/v1/tr.json/translate'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0'
+}
 
 
-def translate(text: str, from_lang: str, to_lang: str):
+def translate(text: str, from_lang: str, to_lang: str, debug: bool = False) -> str:
     params = {
-        'id': '17dd4030d4c560c5-28-0',
+        'id': '1813dbffd7d74abc-1-0',
         'srv': 'yabrowser',
         'text': text,
         'lang': f'{from_lang}-{to_lang}',
         'format': 'html'
     }
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0'
-    }
-    response = requests.get('https://browser.translate.yandex.net/api/v1/tr.json/translate', params=params, headers=headers)
+
+    response = requests.get(url, params=params, headers=headers)
+    if debug:
+        print(response.text)
     return response.json().get('text')[0]
+
+
+async def async_translate(text: str, from_lang: str, to_lang: str, debug: bool = False) -> str:
+    params = {
+        'id': '1813dbffd7d74abc-1-0',
+        'srv': 'yabrowser',
+        'text': text,
+        'lang': f'{from_lang}-{to_lang}',
+        'format': 'html'
+    }
+
+    connector = aiohttp.TCPConnector(ssl=False)
+    session = aiohttp.ClientSession(connector=connector)
+
+    response = await session.get(url, params=params, headers=headers)
+
+    await connector.close()
+    await session.close()
+
+    if debug:
+        print(await response.text())
+
+    return (await response.json()).get('text')[0]
